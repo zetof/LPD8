03cc5a8c204a3784702b4dc4b313b0f9c21f7458
Christophe Bury <christophe.bury@netaxis.be>
2020-03-29 17:04:40 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lpd8",
+    version="1.0.0",
+    author="Christophe Bury",
+    author_email="zetof@zetof.net",
+    description="A Python library to drive an AKAI LPD8 pad controller",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zetof/LPD8",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
